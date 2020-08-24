d88691fa957f41d94b44fac5d74f4573b42d3127
lakegreene <lakegreene@yahoo.com>
2020-08-24 10:30:20 -0400

    Create methods and test husks for JerkSONParser

--- src/main/java/JerkSONParser.java
+++ src/main/java/JerkSONParser.java
@@ -1,2 +1,30 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class JerkSONParser {
+    private String jerkSON;
+
+    public JerkSONParser(String jerkSON) {
+        this.jerkSON = jerkSON;
+    }
+
+    public void findName() {
+        changeName(createMatcher(""), "");
+    }
+
+    public Matcher createMatcher(String pattern) {
+        return Pattern.compile(pattern).matcher(getJerkSON());
+    }
+
+    public void changeName(Matcher matcher, String nameReplacement) {
+        setJerkSON(matcher.replaceAll(nameReplacement));
+    }
+
+    public String getJerkSON() {
+        return jerkSON;
+    }
+
+    public void setJerkSON(String jerkSON) {
+        this.jerkSON = jerkSON;
+    }
 }
--- src/test/java/JerkSONParserTest.java
+++ src/test/java/JerkSONParserTest.java
@@ -1,9 +1,33 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 class JerkSONParserTest {
+JerkSONParser testJerkSON;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws Exception {
+        testJerkSON = new JerkSONParser((new Main()).readRawDataToString());
+    }
+
+    @Test
+    void findName() {
+    }
+
+    @Test
+    void createMatcher() {
+    }
+
+    @Test
+    void changeName() {
+    }
+
+    @Test
+    void getJerkSON() {
+
+    }
+
+    @Test
+    void setJerkSON() {
     }
 }
